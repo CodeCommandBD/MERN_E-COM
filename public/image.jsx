f7f0c7f0bf6923c9fa4b17c18d67c0ce40a4f52b
shanto <shantokumar@example.com>
2025-11-27 23:09:10 +0600

    update

--- public/image.jsx
+++ public/image.jsx
@@ -4,6 +4,6 @@ import verified_gif from './assets/images/verified.gif'
 import verification_fail from './assets/images/verification-failed.gif'
 import loader from './assets/images/loading.svg'
 import imagePlaceholder from './assets/images/img-placeholder.webp'
+import userIcon from './assets/images/user.png'
 
-
-export {logo_white, logo_black, verified_gif, verification_fail, loader, imagePlaceholder}
+export {logo_white, logo_black, verified_gif, verification_fail, loader, imagePlaceholder, userIcon}
