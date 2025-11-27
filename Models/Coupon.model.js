522fa0a25fa1c688af9527c9a0c3034dbf12af51
shanto <shantokumar@example.com>
2025-11-27 17:04:43 +0600

    update

---
+++ Models/Coupon.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const couponSchema =  new mongoose.Schema({
+    coupon_code: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    discountPercentage: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    miniShoppingAmount: {
+        type: Number,
+        required: false,
+        trim: true
+    },
+    validity: {
+        type: Date,
+        required: true,
+    },
+    deletedAt: {
+        type: Date,
+        default: null,
+        index: true
+    },
+
+
+},{timestamps: true})
+
+
+
+const couponModel = mongoose.models['COUPON'] || mongoose.model('COUPON', couponSchema, 'coupons')
+
+export default couponModel
