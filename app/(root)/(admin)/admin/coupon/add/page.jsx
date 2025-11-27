72b02f9cbb82fadd437b137fe4eff348a1423657
shanto <shantokumar@example.com>
2025-11-27 17:27:46 +0600

    update

---
+++ app/(root)/(admin)/admin/coupon/add/page.jsx
@@ -0,0 +1,171 @@
+"use client";
+import BreadCrumb from "@/components/Application/Admin/BreadCrumb";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ADMIN_COUPON_SHOW, ADMIN_DASHBOARD} from "@/Routes/AdminPanelRoute";
+import React, {  useState } from "react";
+import { ButtonLoading } from "@/components/Application/ButtonLoading";
+import { zSchema } from "@/lib/zodSchema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const breadcrumbData = [
+  {
+    href: ADMIN_DASHBOARD,
+    label: "Home",
+  },
+  {
+    href: ADMIN_COUPON_SHOW,
+    label: "Coupon",
+  },
+  {
+    href: "",
+    label: "Add Coupon",
+  },
+];
+
+const AddCoupon = () => {
+  const [loading, setLoading] = useState(false);
+
+
+  // TODO:##### Form valid
+  // TODO:##### Form valid
+  const formSchema = zSchema.pick({
+    name: true,
+    slug: true,
+    category: true,
+    mrp: true,
+    sellingPrice: true,
+    discountPercentage: true,
+    // media: true,
+    description: true,
+  });
+
+  // TODO: ########## Form Define
+  // TODO: ########## Form Define
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      slug: "",
+      description: "",
+      mrp: "",
+      category: "",
+      sellingPrice: "",
+      discountPercentage: "",
+      // media: [],
+    },
+  });
+
+
+
+
+
+
+  const onSubmit = async (value) => {
+    try {
+
+      const { data: response } = await axios.post("/api/coupon/create", value);
+
+      if (!response.success) {
+        throw new Error(response.message);
+      }
+
+      form.reset();
+      showToast("success", response.message);
+
+    } catch (error) {
+      showToast("error", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <BreadCrumb breadcrumbData={breadcrumbData}></BreadCrumb>
+      <Card
+        className="py-0 rounded shadow-sm border"
+        suppressHydrationWarning={true}
+      >
+        <CardHeader
+          className="pt-3 px-3 pb-1"
+          style={{ borderBottom: "1px solid #e5e7eb" }}
+          suppressHydrationWarning={true}
+        >
+          <h4 className="text-xl font-semibold">Add Coupon</h4>
+        </CardHeader>
+        <CardContent className={"py-5"} suppressHydrationWarning={true}>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid md:grid-cols-2 gap-5 ">
+                <div className="mb-5">
+                  <FormField
+                    control={form.control}
+                    name="code"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Coupon Code <span className="text-red-500">*</span>
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            type="text"
+                            placeholder="Enter Coupon Code"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="mb-5">
+                  <FormField
+                    control={form.control}
+                    name="discountPercentage"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          Discount Percentage{" "}
+                          <span className="text-red-500">*</span>
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            readOnly
+                            type="number"
+                            placeholder="00"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>  
+              </div>
+              <div className="mt-5 ">
+                <ButtonLoading
+                  loading={loading}
+                  type={"submit"}
+                  text={"Add Coupon"}
+                  className={"cursor-pointer duration-300"}
+                ></ButtonLoading>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AddCoupon;
