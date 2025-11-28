7c49d0179a10b6b3ea73fe94b820d91a36c8cbe2
shanto <shantokumar@example.com>
2025-11-28 15:54:21 +0600

    update

--- app/(root)/(admin)/admin/dashboard/OrderDetails.jsx
+++ app/(root)/(admin)/admin/dashboard/OrderDetails.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { OrderOverView } from "./OrderOverView";
 import { OrderStatus } from "./OrderStatus";
+import LatestOrder from "./LatestOrder";
 
 const OrderDetails = () => {
   return (
-    
-    <div className="mt-10 flex lg:flex-nowrap flex-wrap gap-10">
+    <div>
+      <div className="mt-10 flex lg:flex-nowrap flex-wrap gap-10">
         <Card className="rounded-lg lg:w-[70%] w-full">
           <CardHeader className="flex justify-between px-4  border-b ">
             <div className="flex items-center justify-between w-full">
@@ -36,7 +37,34 @@ const OrderDetails = () => {
           </CardContent>
         </Card>
       </div>
-  )
-}
+      <div className="mt-10 flex lg:flex-nowrap flex-wrap gap-10">
+        <Card className="rounded-lg lg:w-[70%] w-full">
+          <CardHeader className="flex justify-between px-4  border-b ">
+            <div className="flex items-center justify-between w-full">
+              <span className=" font-semibold">Latest Order</span>
+              <Button type="button" className="text-sm">
+                <Link href="/admin/orders">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <LatestOrder />
+          </CardContent>
+        </Card>
+        <Card className="rounded-lg lg:w-[30%] w-full">
+          <CardHeader className="flex justify-between px-4 border-b">
+            <div className="flex items-center justify-between w-full">
+              <span className=" font-semibold">Latest Review</span>
+              <Button type="button" className="text-sm">
+                <Link href="/admin/orders/summary">View All</Link>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
-export default OrderDetails
+export default OrderDetails;
