9bdd4572ed6513371b6930954c3613bd5478ccd6
shanto <shantokumar@example.com>
2025-11-28 23:37:04 +0600

    update

---
+++ components/Application/Admin/AdminSearch.jsx
@@ -0,0 +1,31 @@
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
+import SearchModel from "./SearchModel";
+
+const AdminSearch = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="md:w-[350px] w-ful ">
+      <div className="relative flex items-center justify-between">
+        <Input
+          placeholder="Search....."
+          readOnly
+          className={"rounded-full coursor-pointer"}
+          onClick={() => setOpen(true)}
+        />
+        <button
+          type="button"
+          className="absolute right-3 top-3 cursor-pointer"
+          onClick={() => setOpen(true)}
+        >
+          <IoIosSearch />
+        </button>
+      </div>
+
+      <SearchModel open={open} setOpen={setOpen} />
+    </div>
+  );
+};
+
+export default AdminSearch;
--- components/Application/Admin/Topbar.jsx
+++ components/Application/Admin/Topbar.jsx
@@ -5,13 +5,14 @@ import UserDropDown from './UserDropDown'
 import { Button } from '@/components/ui/button'
 import { RiMenu4Fill } from "react-icons/ri";
 import { useSidebar } from '@/components/ui/sidebar';
+import AdminSearch from './AdminSearch'
 
 const TopBar = () => {
     const {toggleSidebar} = useSidebar()
     return (
     <div className='fixed border h-14 w-full top-0 left-0 z-30 md:pl-72 md:pr-10 px-5 flex justify-between items-center bg-white dark:bg-card'>
         <div>
-            search components
+            <AdminSearch/>
         </div>
         <div className='flex items-center gap-2'>
             <ThemeSwitch></ThemeSwitch>
