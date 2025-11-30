e83f4b38b65bad7a549606adcaab1869c356d524
shanto <shantokumar@example.com>
2025-11-30 14:59:13 +0600

    update

--- components/Application/Website/MainSlider.jsx
+++ components/Application/Website/MainSlider.jsx
@@ -1,19 +1,22 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import Image from 'next/image';
-import { sliderFour, sliderOne, sliderThree, sliderTwo } from '@/public/image';
+import Image from "next/image";
+import { sliderFour, sliderOne, sliderThree, sliderTwo } from "@/public/image";
 import { LuChevronRight } from "react-icons/lu";
 import { LuChevronLeft } from "react-icons/lu";
-
 
 function SampleNextArrow(props) {
   const { onClick } = props;
   return (
-    <button type='button' className='w-14 h-14 flex items-center justify-center absolute right-0 top-1/2 z-10 bg-white rounded-full hover:bg-white/80 transition-all duration-300 ease-in-out cursor-pointer' onClick={onClick}>
-        <LuChevronRight/>
+    <button
+      type="button"
+      className="w-14 h-14 flex items-center justify-center absolute right-0 top-1/2 z-10 bg-white rounded-full hover:bg-white/80 transition-all duration-300 ease-in-out cursor-pointer"
+      onClick={onClick}
+    >
+      <LuChevronRight />
     </button>
   );
 }
@@ -21,47 +24,79 @@ function SampleNextArrow(props) {
 function SamplePrevArrow(props) {
   const { onClick } = props;
   return (
-    <button type='button' className='w-14 h-14 flex items-center justify-center absolute left-0 top-1/2 z-10 bg-white rounded-full hover:bg-white/80 transition-all duration-300 ease-in-out cursor-pointer' onClick={onClick}>
-        <LuChevronLeft/>
+    <button
+      type="button"
+      className="w-14 h-14 flex items-center justify-center absolute left-0 top-1/2 z-10 bg-white rounded-full hover:bg-white/80 transition-all duration-300 ease-in-out cursor-pointer"
+      onClick={onClick}
+    >
+      <LuChevronLeft />
     </button>
   );
 }
 
-
 const MainSlider = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        autoplay: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
-        cssEase: "linear",
-        nextArrow: <SampleNextArrow />, 
-        prevArrow: <SamplePrevArrow />
-    };
-
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          dots: false,
+          arrows: false,
+          nextArrow: "",
+          prevArrow: "",
+        },
+      },
+    ],
+  };
 
   return (
     <div>
-        <Slider {...settings}>
-            <div>
-                <Image src={sliderOne} alt="sliderOne" width={sliderOne.width} height={sliderOne.height}/>
-            </div>
-            <div>
-                <Image src={sliderTwo} alt="sliderTwo" width={sliderTwo.width} height={sliderTwo.height}/>
-            </div>
-            <div>
-                <Image src={sliderThree} alt="sliderThree" width={sliderThree.width} height={sliderThree.height}/>
-            </div>
-            <div>
-                <Image src={sliderFour} alt="sliderFour" width={sliderFour.width} height={sliderFour.height}/>
-            </div>
-        </Slider> 
+      <Slider {...settings}>
+        <div>
+          <Image
+            src={sliderOne}
+            alt="sliderOne"
+            width={sliderOne.width}
+            height={sliderOne.height}
+          />
+        </div>
+        <div>
+          <Image
+            src={sliderTwo}
+            alt="sliderTwo"
+            width={sliderTwo.width}
+            height={sliderTwo.height}
+          />
+        </div>
+        <div>
+          <Image
+            src={sliderThree}
+            alt="sliderThree"
+            width={sliderThree.width}
+            height={sliderThree.height}
+          />
+        </div>
+        <div>
+          <Image
+            src={sliderFour}
+            alt="sliderFour"
+            width={sliderFour.width}
+            height={sliderFour.height}
+          />
+        </div>
+      </Slider>
     </div>
-  )
-}
+  );
+};
 
-export default MainSlider
+export default MainSlider;
--- public/image.jsx
+++ public/image.jsx
@@ -9,6 +9,9 @@ import sliderOne from './assets/images/slider-1.png'
 import sliderTwo from './assets/images/slider-2.png'
 import sliderThree from './assets/images/slider-3.png'
 import sliderFour from './assets/images/slider-4.png'
+import bannerOne from './assets/images/banner1.png'
+import bannerTwo from './assets/images/banner2.png'
 
 
-export {logo_white, logo_black, verified_gif, verification_fail, loader, imagePlaceholder, userIcon, sliderOne, sliderTwo, sliderThree, sliderFour}
+
+export {logo_white, logo_black, verified_gif, verification_fail, loader, imagePlaceholder, userIcon, sliderOne, sliderTwo, sliderThree, sliderFour, bannerOne, bannerTwo}
