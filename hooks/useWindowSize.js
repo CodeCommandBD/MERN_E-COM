7de82d42843ae9cc69abd2404240dfd51544497d
shanto <shantokumar22@gmail.com>
2025-12-01 12:39:38 +0600

    feat: Implement shop page with product sorting, display limits, and responsive filtering using a new window size hook.

---
+++ hooks/useWindowSize.js
@@ -0,0 +1,23 @@
+import { useState, useLayoutEffect } from "react";
+
+const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: null,
+        height: null,
+    });
+    useLayoutEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    
+    return windowSize;
+};
+export default useWindowSize;
